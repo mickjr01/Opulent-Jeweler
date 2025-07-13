b6f0a927765c46b9c29ad173071a3110284f480e
Dermick Vaughn Jr <dermickvaughnjr@Dermicks-MacBook-Air.local>
2025-07-13 15:04:34 -0700

    Use environment variable for Stripe key

--- public/sever.js
+++ public/sever.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cors = require('cors');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
