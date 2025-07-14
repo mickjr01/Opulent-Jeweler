6b7fa8a5ba8dfb56ddd281638127f93e9497140e
Dermick Vaughn Jr <dermickvaughnjr@Dermicks-MacBook-Air.local>
2025-07-13 19:25:54 -0700

    Deploy working checkout with full layout

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from public folder
 app.use(express.static('public'));
 
+// Serve prices.json data
+app.get('/prices', (req, res) => {
+  res.sendFile(__dirname + '/prices.json');
+});
+
+// Serve index.html on root
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -11,4 +18,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
